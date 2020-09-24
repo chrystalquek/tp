9ed04f0b0572ac550e113cdf17e25d74518f4b45
chrystalquek <chrystalquek@yahoo.com.sg>
2020-09-24 21:19:11 +0800

    Revert tutorials

--- src/test/java/seedu/address/model/person/AddressBookTest.java
+++ src/test/java/seedu/address/model/person/AddressBookTest.java
@@ -1,4 +1,4 @@
-package seedu.address.model;
+package seedu.address.model.person;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
